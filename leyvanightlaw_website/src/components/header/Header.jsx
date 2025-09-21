1e3025d72e5b1ba93c84139470fe26afcbd31e8d
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2025-09-21 11:10:14 -0700

    Inital structure upload

---
+++ leyvanightlaw_website/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.jsx";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Welcome to Leyva Night Law</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ leyvanightlaw_website/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Leyva Night Law</h1>
+      <p>Your trusted legal partner.</p>
+    </div>
+  );
+};
+
+export default Home;
