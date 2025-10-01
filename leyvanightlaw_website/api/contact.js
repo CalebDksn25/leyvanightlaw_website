08e9d21b71de6c2fa141a73026dfbfd3a656e607
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2025-09-30 17:08:45 -0700

    another email api update

--- leyvanightlaw_website/api/contact.js
+++ leyvanightlaw_website/api/contact.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     `;
 
     const result = await resend.emails.send({
-      from: email,
+      from: "mail.leyvanightlaw.com",
       to: ["caleb.n.dickson@gmail.com"],
       reply_to: email,
       subject: `NEW FORM SUBMISSION: ${subject || serviceType || "New Message"}`,
