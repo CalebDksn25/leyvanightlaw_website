c531d11e88014a565e0f19ed8015f0a441109d9c
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2025-09-30 17:15:25 -0700

    onboarding update change

--- leyvanightlaw_website/api/contact.js
+++ leyvanightlaw_website/api/contact.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     `;
 
     const result = await resend.emails.send({
-      from: "caleb.n.dickson@gmail.com",
+      from: "onboarding@resend.dev",
       to: ["caleb.n.dickson@gmail.com"],
       reply_to: email,
       subject: `NEW FORM SUBMISSION: ${subject || serviceType || "New Message"}`,
